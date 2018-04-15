6d37529a164369ff5d3705e4f39e876853ed7c47
razzlepdx <rwilson.or@gmail.com>
2018-04-14 23:54:51 -0700

    add polls Questions to admin '

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +10,16 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        """ Returns helpful identifiers about a question object. """
+        return self.question_text
+
+    def was_published_recently(self):
+        """ Returns a boolean representing questions published
+        in the past 24 hours."""
+
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     """ Defines the class of Choice objects. """
@@ -14,3 +27,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """ Returns helpful identifiers about a Choice object. """
+        return self.choice_text
